bdb9602d96f3da8ca94d2ac5c7e4f058837d6bff
Samuel <sam.lucas.silva@gmail.com>
2024-12-02 14:15:24 -0300

    codificacao e finalizacao

--- Caixa.java
+++ Caixa.java
@@ -2,4 +2,18 @@ package caixa;
 
 public class Caixa {
 
+	private double saldo;
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = this.saldo + saldo;
+	}
+
+	public void Sacar(double x) {
+		this.saldo = this.saldo - x;
+	}
+
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,51 @@
 package caixa;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Caixa caixa = new Caixa();
 
+		Scanner scanner = new Scanner(System.in);
+
+		Caixa caixaBanco = new Caixa();
+
+		int escolha;
+
+		System.out.println("Bem-vindo");
+
+		do {
+
+			System.out.println("1. Depositar ou adicionar fundos");
+			System.out.println("2. Sacar dinheiro");
+			System.out.println("3. Consultar o saldo");
+			System.out.println("4. Sair");
+
+			escolha = scanner.nextInt();
+
+			switch (escolha) {
+			case 1:
+				System.out.println("Qual é o valor?");
+				double valor = scanner.nextDouble();
+				caixaBanco.setSaldo(valor);
+				break;
+			case 2:
+				System.out.println("Qual é o valor a ser retirado?");
+				double valorSaque = scanner.nextDouble();
+				if (valorSaque <= caixaBanco.getSaldo()) {
+					caixaBanco.Sacar(valorSaque);
+				} else {
+					System.out.println("Você não possui saldo o suficiente para realizar essa operação");
+				}
+				break;
+			case 3:
+				System.out.printf("O valor em sua conta é de R$ %.2f %n", caixaBanco.getSaldo());
+				break;
+			}
+
+		} while (escolha != 4);
+
+		scanner.close();
 	}
 
 }
